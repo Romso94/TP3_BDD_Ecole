c22dd648159a6952e2253bd5b8767379baad5adb
Romso <89916346+Romso94@users.noreply.github.com>
2024-05-06 14:45:55 +0200

    Update tp3.sql

--- tp3.sql
+++ tp3.sql
@@ -1,22 +1,19 @@
 ###
 START TRANSACTION;
-
-INSERT INTO eleves (NUM_ELEVE,NOM,PRENOM,DATE_NAISSANCE,POIDS,ANNEE,GENRE) VALUES (11,"Michel","Michel",'2000-5-18',79,2,"M");
+INSERT INTO eleves (NUM_ELEVE,NOM,PRENOM,DATE_NAISSANCE,POIDS,ANNEE,GENRE) VALUES (11,"eleve","1",'2000-5-18',79,2,"M");
 SELECT * from eleves;
 ROLLBACK;
 SELECT * from eleves;
 
-Avec l'instruction ROLLBACK On modifie notre table mais on ne sauvegarde pas dans celle-ci les nouvelles valeurs
+--Avec l'instruction ROLLBACK On modifie notre table mais on ne sauvegarde pas dans celle-ci les nouvelles valeurs
 
 
 ###
-
 START TRANSACTION;
-
-INSERT INTO eleves (NUM_ELEVE,NOM,PRENOM,DATE_NAISSANCE,POIDS,ANNEE,GENRE) VALUES (11,"Michel","Michel",'2000-5-18',79,2,"M");
+INSERT INTO eleves (NUM_ELEVE,NOM,PRENOM,DATE_NAISSANCE,POIDS,ANNEE,GENRE) VALUES (11,"eleve","1",'2000-5-18',79,2,"M");
 SELECT * from eleves;
 COMMIT;
 SELECT * from eleves;
 
-Avec le commit On enregistre dans notre base de donné la nouvelle ligne ajouté 
+--Avec le commit on enregistre dans notre base de donné la nouvelle ligne ajouté 
 
